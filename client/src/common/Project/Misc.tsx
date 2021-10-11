6ac2b3fb6fc5335dae0743804444a13492ff4cbb
Dan Abreo <dabreo43@gmail.com>
2021-10-10 19:02:06 -0500

    add majority of submission page

--- client/src/common/Project/Misc.tsx
+++ client/src/common/Project/Misc.tsx
@@ -1,4 +1,4 @@
-import { Button, Input, Radio, Spacer, Select, useToasts, Text } from "@geist-ui/react";
+import { Button, Card, Divider, Input, Radio, Spacer, Select, useToasts, Text } from "@geist-ui/react";
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { BASE_URL } from "../../constants";
@@ -37,7 +37,6 @@ export const ProjectPage: React.FC = () => {
     const userAuthId = "5efc0b99a37c4300032acbce"
     
     const [curEventId, setCurEventId] = useState<string>("");
-    const [submissionId, setSubmissionId] = useState<string>("");
 
     const [allChalleges, setAllChallenges] = useState<Challenge[]>([])
     useEffect(() => {
@@ -56,39 +55,36 @@ export const ProjectPage: React.FC = () => {
     },[curEventId])
 
     const [submission, setSubmission] = useState<Submission>();
-    useEffect(() => {
-      let mounted = true
-      if (curEventId) {
-        axios.get<SubmissionResponse>(`${BASE_URL}/api/${curEventId}/submission/${submissionId}`)
-        .then(res => {
-          if (mounted) {
-            setSubmission(res.data.result)
-          }
-        });
-      }
-      return () => {
-        mounted = false
-      }
-    }, [curEventId, submissionId])
     const submissionDataHandler = (e:any) => setSubmission((prev:any) => (
       {
         ...prev,
         [e.target.id]: ((e.target.id === "name" || e.target.id === "videoLink") ? e.target.value : e.target.value.split(','))
       }));
 
-    const [editable, setEditable] = useState(false);
-    const handleEditButton = () => {
-      setEditable(prev => {
-        if (prev) {
-          axios.post(`${BASE_URL}/api/${curEventId}/submission/add`, submission)
-          .then(() => sendNotification("Updated submission!", "success"))
-          .catch(res => {
-            sendNotification(String(res.response.data.error), "error");
-          })
-        }
-        return !prev
-      });
-    }
+      const emptyCurSubmission = () => setSubmission({
+        _id: "",
+        name: "",
+        tags: [],
+        links: [],
+        discordTags: [],
+        challengeIds: [],
+        videoLink: "",
+        answer1: "",
+        answer2: "",
+        answer3: "",
+        answer4: "",
+        answer5: "",
+      })
+
+    const handleUpdateSubmission = () => {
+      axios.post(`${BASE_URL}/api/${curEventId}/submission/add`, submission)
+      .then(() => {
+        sendNotification("Updated submission!", "success")
+        emptyCurSubmission();
+      })
+      .catch(res => {
+        sendNotification(String(res.response.data.error), "error");
+      })};
 
     const [eventList, setEventList] = useState<Event[]>([]);
     useEffect(() => {
@@ -103,6 +99,11 @@ export const ProjectPage: React.FC = () => {
         mounted = false
       }
     },[]);
+
+    const handleEditSubmission = (id:string) => {
+      axios.get<SubmissionResponse>(`${BASE_URL}/api/${curEventId}/submission/${id}`)
+      .then(res => setSubmission(res.data.result));
+    }
 
     const [submissions, setSubmissions] = useState<Submission[]>([])
     useEffect(() => {
@@ -120,26 +121,6 @@ export const ProjectPage: React.FC = () => {
       }
     },[curEventId])
 
-    const setSubmissionHandler = (val:any) => {
-      if (val === "create_new_submission") {
-        const date_string = new Date().toISOString();
-        axios.post(`${BASE_URL}/api/${curEventId}/submission/add`, {
-          name: `New Submission created at ${date_string}`,
-          discordTags: ['default'],
-          videoLink: "dummylink",
-          challengeIds: []
-        })
-        .then(res => {
-          sendNotification("Added new submission!", "success");
-        })
-        .catch(res => {
-          sendNotification(String(res.response.data.error), "error");
-        })
-      } else {
-        setSubmissionId((val as string));
-      }
-    };
-
     return (
     <>
       <Select placeholder="Select an Event" onChange={val => setCurEventId((val as string))}>
@@ -148,46 +129,53 @@ export const ProjectPage: React.FC = () => {
         })}
       </Select>
       <Spacer h={1}/>
-      <Select placeholder="Add/Select a Submission" onChange={setSubmissionHandler}>
-        <Select.Option value="create_new_submission">Create a new submission</Select.Option>
-        <Select.Option divider />
-        {submissions.filter(submission => (submission && submission._id))
-        .map(submission => (
-          <Select.Option key={submission._id} value={submission._id}>{submission.name}</Select.Option>
-        ))}
-      </Select>
-      {curEventId && submissionId &&
-      <>
-      <Text>{JSON.stringify(submission)}</Text>
+      {curEventId &&
+      <Card>
+        <Card.Content>
+        <Text b>Add/Update a Submission</Text>
+        </Card.Content>
+        <Divider />
+        <Card.Content>
+        <Input width="100%" key="name" label="Name" value={submission?.name} id="name" onChange={submissionDataHandler}/>
         <Spacer h={1}/>
-        <Input width="100%" label="Name" disabled={!editable} value={submission?.name} id="name" onChange={submissionDataHandler}/>
+        <Input width="100%" key="tags" label="Tags" value={submission?.tags?.join()} id="tags" onChange={submissionDataHandler}/>
         <Spacer h={1}/>
-        <Input width="100%" label="Tags" disabled={!editable} value={submission?.tags?.join()} id="tags" onChange={submissionDataHandler}/>
+        <Input width="100%" key="links" label="Links" value={submission?.links?.join()} id="links" onChange={submissionDataHandler}/>
         <Spacer h={1}/>
-        <Input width="100%" label="Links" disabled={!editable} value={submission?.links?.join()} id="links" onChange={submissionDataHandler}/>
+        <Input width="100%" key="discordtags" label="Discord Tags" value={submission?.discordTags?.join()} id="discordTags" onChange={submissionDataHandler}/>
         <Spacer h={1}/>
-        <Input width="100%" label="Discord Tags" disabled={!editable} value={submission?.discordTags?.join()} id="discordTags" onChange={submissionDataHandler}/>
-        <Spacer h={1}/>
-        <Input width="100%" label="Video Link" disabled={!editable} value={submission?.videoLink} id="videoLink" onChange={submissionDataHandler}/>
+        <Input width="100%" key="videolink" label="Video Link" value={submission?.videoLink} id="videoLink" onChange={submissionDataHandler}/>
         <Spacer h={1}/>
         <Text>Select a challenge to submit this project to:</Text>
-        <Radio.Group useRow disabled={!editable} value={submission?.challengeIds?.at(0)} onChange={(c:any) => setSubmission((prev:any) => ({...prev, challengeIds: [c]}))}>
+        <Radio.Group useRow value={submission?.challengeIds?.at(0)} onChange={(c:any) => setSubmission((prev:any) => ({...prev, challengeIds: [c]}))}>
           {allChalleges.map(challenge => <Radio key={challenge._id} value={challenge._id}>{challenge.name}</Radio>)}
         </Radio.Group>
-        <Text>{JSON.stringify(submission?.challengeIds?.at(0))}</Text>
         <Spacer h={1}/>
         {allChalleges.filter(challenge => challenge._id === submission?.challengeIds?.at(0)).map(challenge =>
             <>
-              {challenge.question1 && <Input width="100%" label={challenge?.question1} disabled={!editable} value={submission?.answer1} key="answer1" id="answer1" onChange={submissionDataHandler}/>}
-              {challenge.question2 && <Input width="100%" label={challenge?.question2} disabled={!editable} value={submission?.answer2} key="answer2" id="answer2" onChange={submissionDataHandler}/>}
-              {challenge.question3 && <Input width="100%" label={challenge?.question3} disabled={!editable} value={submission?.answer3} key="answer3" id="answer3" onChange={submissionDataHandler}/>}
-              {challenge.question4 && <Input width="100%" label={challenge?.question4} disabled={!editable} value={submission?.answer4} key="answer4" id="answer4" onChange={submissionDataHandler}/>}
-              {challenge.question5 && <Input width="100%" label={challenge?.question5} disabled={!editable} value={submission?.answer5} key="answer5" id="answer5" onChange={submissionDataHandler}/>}
+              {challenge.question1 && <Input width="100%" label={challenge?.question1} value={submission?.answer1} key="answer1" id="answer1" onChange={submissionDataHandler}/>}
+              {challenge.question2 && <Input width="100%" label={challenge?.question2} value={submission?.answer2} key="answer2" id="answer2" onChange={submissionDataHandler}/>}
+              {challenge.question3 && <Input width="100%" label={challenge?.question3} value={submission?.answer3} key="answer3" id="answer3" onChange={submissionDataHandler}/>}
+              {challenge.question4 && <Input width="100%" label={challenge?.question4} value={submission?.answer4} key="answer4" id="answer4" onChange={submissionDataHandler}/>}
+              {challenge.question5 && <Input width="100%" label={challenge?.question5} value={submission?.answer5} key="answer5" id="answer5" onChange={submissionDataHandler}/>}
             </>
         )}
-        <Button onClick={handleEditButton}>{editable ? "Update" : "Edit"}</Button>      
-        </>
+        <Spacer h={1}/>
+        <Button onClick={handleUpdateSubmission}>{submission?._id ? "Update" : "Add"}</Button>    
+        </Card.Content>
+      </Card>
       }
+      <Spacer h={0.5}/>
+      {submissions.filter(submission => (submission && submission._id)).map(submission => (
+          <React.Fragment key={submission._id}>
+            <Card>
+              <Text>{submission.name}</Text>
+              <Button auto scale={0.5} value={submission._id} onClick={() => handleEditSubmission(submission._id)}>Edit</Button>
+            </Card>
+            <Spacer h={0.5}/>
+          </React.Fragment>
+        )
+      )}
     </>
     );
 };
