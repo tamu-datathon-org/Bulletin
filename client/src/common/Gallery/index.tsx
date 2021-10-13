f2e56195c762f591735000c6266a815432adbe69
Dan Abreo <dabreo43@gmail.com>
2021-10-13 18:39:19 -0500

    add gallery

---
+++ client/src/common/Gallery/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from "@geist-ui/react";
+import React from 'react';
+
+
+/**
+ * Entire Gallery page
+ */
+export const GalleryPage: React.FC = () => {
+  return (<Text>View all projects submitted to this event.</Text>)
+};
