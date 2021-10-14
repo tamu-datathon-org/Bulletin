5f77de3b81de95842a6782577bc2de2d0e272bec
Dan Abreo <dabreo43@gmail.com>
2021-10-13 20:31:20 -0500

    view events

--- client/src/common/Gallery/index.tsx
+++ client/src/common/Gallery/index.tsx
@@ -1,10 +1,50 @@
-import { Text } from "@geist-ui/react";
-import React from 'react';
+import { Text, Spinner, Grid, Card, Image } from "@geist-ui/react";
+import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import { BASE_URL } from "../../constants";
+import {EventResponse, Event} from '../interfaces';
+import placeholder from './placeholder.jpg';
 
 
 /**
  * Entire Gallery page
  */
 export const GalleryPage: React.FC = () => {
-  return (<Text>View all projects submitted to this event.</Text>)
+  const EVENT_ID = "61638c45dacc9ccfee024234"
+  const [eventLoaded, setEventLoaded] = useState(false);
+  const [curEvent, setCurEvent] = useState<Event>();
+  useEffect(() => {
+    let mounted = true
+    if (EVENT_ID) {
+      setEventLoaded(false)
+      axios.get<EventResponse>(`${BASE_URL}/api/${EVENT_ID}?full=true`)
+      .then(res => {
+        if (mounted) {
+          setCurEvent(res.data.result)
+          setEventLoaded(true)
+        }
+      })
+    }
+    return () => {
+      mounted = false
+    }
+  }, [])
+  return (<>
+  {eventLoaded
+  ? <Grid.Container gap={2} justify="center" height="100px">
+      {curEvent?.submissions.map(submission => 
+        <Grid xs={24} sm={12} md={8} lg={6} xl={4}>
+          <Card shadow width="100%">
+          <Image
+            src={submission.icon ? submission.icon : placeholder}
+          />
+          <Text b>{submission.name}</Text>
+          <Text b>{submission.icon}</Text>
+          </Card>
+        </Grid>
+      )}
+    </Grid.Container>
+  : <Spinner />
+  }
+  </>)
 };
