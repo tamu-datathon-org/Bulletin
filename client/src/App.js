ccef87802b6d7ec03dcb6b04b0ff17cd110e1b3f
Dan Abreo <dabreo43@gmail.com>
2021-10-15 23:33:54 -0500

    initial router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
-import {Page, Tabs, Spacer} from '@geist-ui/react';  
+import {Page, Spacer} from '@geist-ui/react';  
 import { CookiesProvider } from 'react-cookie';
 import {Navbar} from './common/Navbar';
 import './common/globals.css';
-import {AdminPage} from './common/Admin'
-import {ProjectPage} from './common/Project'
-import {GalleryPage} from './common/Gallery'
+import Switcher from './Switcher'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
@@ -14,23 +17,19 @@ config.autoAddCss = false;
 function App() {
   return (
     <>
-      <CookiesProvider>
-      <Navbar />
-      <Spacer h={3}/>
-      <Page>
-        <Tabs initialValue="1">
-          <Tabs.Item label="Project Gallery" value="1">
-            <GalleryPage />
-          </Tabs.Item>
-          <Tabs.Item label="My Projects" value="2">
-            <ProjectPage />
-          </Tabs.Item>
-          <Tabs.Item label="Admin" value="3">
-            <AdminPage />
-          </Tabs.Item>
-        </Tabs>
-      </Page>
-      </CookiesProvider>
+      <Router>
+        <CookiesProvider>
+        <Navbar />
+        <Spacer h={3}/>
+        <Page>
+        <Switch>
+          <Route path="/bulletin" children={<Switcher />} />
+          <Route path="/bulletin/:id" children={<Switcher />} />
+          {/* <Route path="/bulletin/project/:id" children={<ProjectView />} /> */}
+        </Switch>
+        </Page>
+        </CookiesProvider>
+      </Router>
     </>
   );
 }
