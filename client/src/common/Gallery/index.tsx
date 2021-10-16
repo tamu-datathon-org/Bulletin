12b663e83bc32a80cf94cb9467a5aae0ae388f6e
Dan Abreo <dabreo43@gmail.com>
2021-10-16 02:07:06 -0500

    add bulletin to redirect rul

--- client/src/common/Gallery/index.tsx
+++ client/src/common/Gallery/index.tsx
@@ -39,7 +39,7 @@ export const GalleryPage: React.FC = () => {
   ? <Grid.Container gap={2} justify="center" height="100px">
       {curEvent?.submissions.map(submission => 
         <Grid key={submission._id} xs={24} sm={12} md={8} lg={6} xl={4}>
-          <RouterLink to={`project/${submission._id}`}>
+          <RouterLink to={`bulletin/project/${submission._id}`}>
             <Card shadow width="100%">
             <Image
               src={submission?.icon?.[1] ? submission.icon[1] : placeholder}
